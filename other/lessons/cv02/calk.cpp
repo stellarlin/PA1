5b1d060a98b026b43ea69029e7a79d1bac0f4b6a
Sofiia Prykhach <prykhsof@cvut.cz>
2022-09-15 20:39:25 +0200

    cv2: calk is done

--- other/lessons/cv02/calk.cpp
+++ other/lessons/cv02/calk.cpp
@@ -2,4 +2,52 @@
 // Created by София Прихач on 15.09.2022.
 //
 
-#include <
+#include <cstdio>
+#include <climits>
+#include <cmath>
+
+int error () {
+    printf("Nespravny vstup.\n");
+    return 0;
+}
+
+int main()
+{
+    int a =0, b = 0;
+    unsigned int s;
+    if(scanf("%d %d", &a, &b)!=2) return error();
+
+    //--------------------------------------------//
+    //sum//
+    if ((a > 0 & b >0) )
+    {
+        s =abs(a)+abs(b);
+        if ( s>INT_MAX) return error();
+    }
+    if ((a < 0 & b <0) )
+    {
+        s =abs(a)+abs(b);
+        if ( s>abs(INT_MIN)) return error();
+    }
+
+    printf("%d + %d = %d\n", a, b, a+b);
+
+    //--------------------------------------------//
+    if ((a > 0 & b <0) )
+    {
+        s =abs(a)+abs(b);
+        if ( s>INT_MAX) return error();
+    }
+    if ((a < 0 & b >0) )
+    {
+        s =abs(a)+abs(b);
+        if ( s>abs(INT_MIN)) return error();
+    }
+    printf("%d - %d = %d\n", a, b, a-b);
+    //--------------------------------------------//
+    if (b==0)return error();
+    printf("%d / %d = %d\n", a, b, a/b);
+    int c = a % b;
+    printf("%d %% %d = %d\n", a, b, c);
+    return 0;
+}
