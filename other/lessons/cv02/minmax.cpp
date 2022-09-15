0e5948cf2f313c3102e0681efbe59270b9c2507f
Sofiia Prykhach <prykhsof@cvut.cz>
2022-09-15 21:06:45 +0200

    cv2: minmax.cpp is done

--- other/lessons/cv02/minmax.cpp
+++ other/lessons/cv02/minmax.cpp
@@ -1,3 +1,25 @@
 //
 // Created by София Прихач on 15.09.2022.
 //
+#include <cstdio>
+
+int error () {
+    printf("Nespravny vstup.\n");
+    return 0;
+}
+
+int main ()
+{
+    int a, b , c, max, min, mid;
+//<Условие> ? <если выполнено> : <если нет>
+    if (scanf("%d %d %d", &a, &b, &c)!=3) return error();
+    max = min = a;
+    b>max ? max = b : max;
+    c>max ? max = c : max;
+    b<min ? min = b : min;
+    c<min ? min = c : min;
+    mid = a + b + c - max - min;
+    printf("max = %d, mid = %d, min = %d\n", max, mid, min);
+
+    return 0;
+}
