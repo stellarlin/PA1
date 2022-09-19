07b6f3b9dcd4ef9f9d942d0774a104ae488a6fc0
Sofiia Prykhach <prykhsof@fit.cvut.cz>
2022-09-19 18:30:08 +0200

    cv1: first.cpp everything is not well

--- other/lessons/cv01/first_code.cpp
+++ other/lessons/cv01/first_code.cpp
@@ -11,8 +11,13 @@ int read_char (char c)
 {
 if (c>='0' && c <= '9') return c - '0';
 if (c>='A' && c <= 'Z') return  c - 'A'+10;
-if (c>='a' && c<='z') return c - 'a'+10;
-return 0;
+else return c - 'a'+10;
+}
+
+char write_char(unsigned int n)
+{
+    if (n >=0 && n<=9) return (char)(n + '0');
+    else return (char) (n +'A' -10);
 }
 
 bool isValid(char * n1, unsigned int b1)
@@ -30,35 +35,64 @@ bool isValid(char * n1, unsigned int b1)
 }
 
 
-unsigned int convert_decimal (char * n1, unsigned int b1)
+unsigned int convert_decimal (char * n1, unsigned int b1, int * exit)
 {
-    unsigned int num=0, size = strlen(n1), z =1;
-    for (int i =  size-1; i>=0; --i)
+    unsigned int num=0, size = strlen(n1),z;
+    for (unsigned int i = size; i>0; --i)
     {
-        z=1;
-        int j = i;
-        while (j>=0)
+        z=1;  long long tmp;
+        unsigned int j = i-1;
+        while (j>0)
         {
-            if(j==0) break;
             z*=b1;
             j--;
         }
-        num+=read_char(n1[i])*z;
+        tmp = (num+read_char(n1[i])*z);
+        if( tmp >UINT_MAX || tmp <0)
+        {*exit =1; return 0;}
+
+        num+=read_char(n1[size-i])*z;
     }
 return num;
 }
 
+void reverse ( char * n2)
+{
+    unsigned int len= strlen(n2);
+    unsigned int i;
+    for (i = 0; i < len/2; i++)
+    {
+        char temp = n2[i];
+        n2[i] = n2[len-i-1];
+        n2[len-i-1] = temp;
+    }
+}
+
+char * convert_base (char * n2, unsigned int n1, unsigned int b2)
+{
+    int i = 0;
+    while (n1 > 0) {
+        n2[i] = write_char(n1 % b2);
+        n1 /= b2;
+        i++;
+    }
+    n2[i] = '\0';
+    reverse(n2);
+    return n2;
+}
 int main ()
 {
-    unsigned  int b1, b2;
-    char n1 [32];
+    int exit = 0;
+    unsigned  int b1, b2, num;
+    char n1 [32], n2[32];
     printf("Zadejte cislo a dve bazi ve formatu: n1 b1 b2\n");
     if(scanf(" %s %u %u", n1, &b1, &b2)!=3
         || b1 >36 || b1<2
         || b2 >36 || b2<2
         || !isValid(n1,b1)) return error();
-    printf("%d\n", convert_decimal(n1, b1));
-
+    num = convert_decimal(n1, b1, &exit);
+    if(exit==1)return error();
+    printf ("%s\n", convert_base(n2,num,b2));
     return 0;
 }
 
