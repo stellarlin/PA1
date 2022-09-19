d517650265fb59e8b779a151529e970a115150c6
Sofiia Prykhach <prykhsof@fit.cvut.cz>
2022-09-19 14:29:16 +0200

    cv1: first fucking hodina

--- other/lessons/cv01/first_code.cpp
+++ other/lessons/cv01/first_code.cpp
@@ -1,5 +1,7 @@
 #include <cstdio>
 #include <climits>
+#include <cstring>
+
 int error () {
     printf("Nespravny vstup.\n");
     return 0;
@@ -10,14 +12,16 @@ int read_char (char c)
 if (c>='0' && c <= '9') return c - '0';
 if (c>='A' && c <= 'Z') return  c - 'A'+10;
 if (c>='a' && c<='z') return c - 'a'+10;
+return 0;
 }
 
 bool isValid(char * n1, unsigned int b1)
 {
     // || n1 >=0 || (b1 ==10 && n2<= UINT_MAX)
     unsigned int num;
-    while (!n1)
+    while (*n1!='\0')
     {
+        if (*n1<'0' || (*n1 >'9' && *n1<'A') || (*n1>'Z' && *n1<'a') || *n1 >'z') return false;
         num = read_char(*n1);
         if (num > b1) return false;
         n1++;
@@ -26,15 +30,35 @@ bool isValid(char * n1, unsigned int b1)
 }
 
 
+unsigned int convert_decimal (char * n1, unsigned int b1)
+{
+    unsigned int num=0, size = strlen(n1), z =1;
+    for (int i =  size-1; i>=0; --i)
+    {
+        z=1;
+        int j = i;
+        while (j>=0)
+        {
+            if(j==0) break;
+            z*=b1;
+            j--;
+        }
+        num+=read_char(n1[i])*z;
+    }
+return num;
+}
+
 int main ()
 {
     unsigned  int b1, b2;
-    char n1 [36];
+    char n1 [32];
     printf("Zadejte cislo a dve bazi ve formatu: n1 b1 b2\n");
     if(scanf(" %s %u %u", n1, &b1, &b2)!=3
-        || b1 <=36 || b1>=2
-        || b2 <=36 || b2>=2) return error();
-    if (b1<=10 && !isValid10(n1,b1)) return error();
+        || b1 >36 || b1<2
+        || b2 >36 || b2<2
+        || !isValid(n1,b1)) return error();
+    printf("%d\n", convert_decimal(n1, b1));
 
     return 0;
 }
+
