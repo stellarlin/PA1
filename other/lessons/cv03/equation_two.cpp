59e7375304f0e562452d06977f31b30e6d81b301
Sofiia Prykhach <prykhsof@fit.cvut.cz>
2022-09-21 15:25:14 +0200

    cv3:  equation_two.cpp done but i am not sure

---
+++ other/lessons/cv03/equation_two.cpp
@@ -0,0 +1,24 @@
+#include <cstdio>
+
+int error () {
+    printf("Nespravny vstup.\n");
+    return 0;
+}
+int main ()
+{
+    double a,b,c,d, e, f;
+    double x, y;
+    printf("Zadejte 2 rovnici ve formatu ax+by=c.\n");
+    printf("Prvni rovnice:\n");
+    if(scanf(" %lfx + %lfy = %lf", &a,&b,&c)!=3) return error();
+    printf("Druha rovnice:\n");
+    if(scanf(" %lfx + %lfy = %lf", &d,&e,&f)!=3
+    || e*a - d*b ==0) return error();
+
+    y = a*f - d*c;
+    y/= (e*a-b*d);
+    x = e*c - f*b;
+    x/=(e*a-b*d);
+    printf ("x = %.2f, y = %.2f", x,y);
+    return 0;
+}
