b8ea23068147fcde9786c6e60ecab7b349163bd1
Sofiia Prykhach <prykhsof@fit.cvut.cz>
2022-09-26 11:38:54 +0200

    cv2: time was corrected

--- other/lessons/cv02/time.cpp
+++ other/lessons/cv02/time.cpp
@@ -16,15 +16,18 @@ int main ()
 {
     time first, second, res;
     int carry = 0;
-    if (scanf("%d:%d %d:%d", &first.hour, &first.minute, &second.hour, &second.minute)!=4 ||
+    printf("Zadejte cas ve formatu \"xx:xx xx:xx\" \n");
+    if (scanf(" %d:%d %d:%d", &first.hour, &first.minute, &second.hour, &second.minute)!=4 ||
             first.hour <0 || first.hour>23 ||second.hour <0 || second.hour>23||
-            first.minute<0 || first.minute>59 || second.minute<0 || second.minute>59||
-            second.hour<first.hour || (second.hour==first.hour && second.minute < first.minute)) return error();
+            first.minute<0 || first.minute>59 || second.minute<0 || second.minute>59) return error();
+    
     second.minute<first.minute
     ? res.minute = second.minute - first.minute +60, carry=1
     : res.minute = second.minute-first.minute;
 
-    res.hour = second.hour-first.hour-carry;
+    second.hour<first.hour
+    ? res.hour = 24 - first.hour +second.hour-carry
+    :res.hour = second.hour-first.hour-carry;
 
     printf("Time difference is %d:%d\n", res.hour, res.minute);
     return 0;
