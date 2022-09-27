f326a152cb1a2f0296cd52340f7cfe07cd5b36dc
Sofiia Prykhach <prykhsof@fit.cvut.cz>
2022-09-27 14:13:38 +0200

    cv4: rovnice different constant is enable

--- other/lessons/cv04/rovnice.cpp
+++ other/lessons/cv04/rovnice.cpp
@@ -7,25 +7,30 @@ int error (void) {
     return 0;
 }
 
-void rovnice (double top, double bottom, double D)
+void rovnice (double top, double bottom, double D, char z)
 {
     double res1, res2;
     res1=(top+sqrt(D))/bottom;
     res2=(top-sqrt(D))/bottom;
-    printf("x1 = %.2f, x2 = %.2f", res1,res2);
+    printf("%c1 = %.2f, %c2 = %.2f\n", z, res1, z, res2);
 }
 
 int main(void)
 {
     double a,b,c,d,e,f;
+    char z1, z2, z3, z4;
     while(!feof(stdin)) {
         printf("Zadejte kvadratickou rovnici ve tvaru a x^2 + b x + c = d x^2 + e x + f:\n");
-        if (scanf(" %lf x^2 + %lf x + %lf = %lf x^2 + %lf x + %lf", &a, &b, &c, &d, &e, &f) != 6) return error();
+        if (scanf(" %lf %c^2 + %lf %c + %lf = %lf %c^2 + %lf %c + %lf", &a, &z1, &b, &z2, &c, &d, &z3, &e, &z4, &f) != 10
+            || z1!=z2 || z1!=z3 || z1!=z4 || z2!=z3 || z2!=z4 || z3!=z4 ||getchar()!='\n') return error();
+        if(z1<'A' || (z1>'Z' && z1<'a') || z1>'z') return error();
+
         if (fabs(b * b - 2 * b * e + e * e - 4 * (a - d) * (c - f)) <= 100 * 4 * (a - d) * (c - f) * DBL_EPSILON)
-            printf("x=%.f.\n", (e - b) / 2 * (a - d));
+            printf("%c=%.f.\n", z1, (e - b) / 2 * (a - d));
         else if (b * b - 2 * b * e + e * e - 4 * (a - d) * (c - f) > 100 * 4 * (a - d) * (c - f) * DBL_EPSILON)
-            rovnice(e - b, 2 * (a - d), b * b - 2 * b * e + e * e - 4 * (a - d) * (c - f));
+            rovnice(e - b, 2 * (a - d), b * b - 2 * b * e + e * e - 4 * (a - d) * (c - f),z1);
         else printf("Neexistuje reseni v R. Zadejte jinou hodnotu\n");
+        printf("\n");
     }
     return 0;
 }
