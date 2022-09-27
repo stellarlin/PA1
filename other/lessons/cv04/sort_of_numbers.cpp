80795b36ff56ad43767f64c8b71a88222e0cc91c
Sofiia Prykhach <prykhsof@fit.cvut.cz>
2022-09-27 10:00:04 +0200

    cv4: sort done

---
+++ other/lessons/cv04/sort_of_numbers.cpp
@@ -0,0 +1,24 @@
+#include<stdio.h>
+int error (void) {
+    printf("Nespravny vstup.\n");
+    return 0;
+}
+
+int main(void)
+{
+    int n, m;
+    while(!feof(stdin)) {
+        printf("Zadejte cisla:\n");
+        if (scanf(" %d %d", &n, &m) != 2 || getchar()!='\n')return error();
+        printf ("n = %d, m = %d\n", n, m);
+       if(n>=m) printf ("Sorted: %d, %d\n",n, m);
+       else printf (" Sorted: %d, %d\n", m, n);
+        printf ("Prumer: %f\n",1.0*(n+m)/2);
+        int tmp = n;
+        n=m;
+        m=tmp;
+        printf ("n = %d, m = %d\n", n, m);
+        return 0;
+    }
+}
+
