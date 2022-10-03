23ee1b423b57d74d9cec24045490631e546fbe50
Sofiia Prykhach <prykhsof@fit.cvut.cz>
2022-10-03 15:07:07 +0200

    cv3: rym_num is fixed

--- other/lessons/cv03/rym_num.cpp
+++ other/lessons/cv03/rym_num.cpp
@@ -15,6 +15,23 @@ void print_num(int num, char c)
     }
 }
 
+void less_ten (int num);
+void less_fifty( int num);
+void less_hundred(int num);
+void less_5hundred(int num);
+void less_thousand(int num);
+void less_4thousand(int num);
+
+void itinerary(int num)
+{
+    if (num <= 10) less_ten(num);
+    else if (num <= 50) less_fifty(num);
+    else if (num<=100) less_hundred(num);
+    else if (num<=500)less_5hundred(num);
+    else if (num<=1000) less_thousand(num);
+    else if (num<4000) less_4thousand(num);
+}
+
 void less_ten (int num)
 {
 
@@ -50,84 +67,75 @@ int carry;
         case 0: break;
     }
 }
-
 void less_fifty( int num)
 {
     if(num==0)return;
     int carry;
     if ((carry=num-50)<=0 && carry >= -10) {
-        less_ten(abs(carry));
+        itinerary(abs(carry));
         print_num(1, 'L');
     }
     else
     {
         print_num(num/10, 'X');
-        less_ten(num%10);
+        itinerary(num%10);
     }
 }
-
 void less_hundred(int num)
 {
     if(num==0)return;
     int carry;
     if ((carry=num-100)<=0 && carry >= -10) {
-        less_ten(abs(carry));
+        itinerary(abs(carry));
         print_num(1, 'C');
     }
     else
     {
         print_num(1, 'L');
         print_num(abs(num-50)/10, 'X');
-        less_ten(abs(num-50)%10);
+        itinerary(abs(num-50)%10);
     }
     }
-
-    void less_5hundred(int num)
+void less_5hundred(int num)
     {
     if(num==0) return;
     int carry;
         if ((carry=num-500)<=0 && carry >= -100) {
-            less_hundred(abs(carry));
+            itinerary(abs(carry));
             print_num(1, 'D');
         }
         else
         {
             print_num(num/100, 'C');
-            less_hundred(num%100);
+            itinerary(num%100);
         }
     }
-
-    void less_thousand(int num)
+void less_thousand(int num)
     {
         if(num==0) return;
         int carry;
         if ((carry=num-1000)<=0 && carry >= -100) {
-            less_5hundred(abs(carry));
+            itinerary(abs(carry));
             print_num(1, 'M');
         }
         else
         {
             print_num(num/500, 'D');
-            less_5hundred(num%500);
+            itinerary(num%500);
         }
     }
 void less_4thousand(int num)
 {
     print_num(num/1000, 'M');
-    less_thousand(num%1000);
+    itinerary(num%1000);
 }
 int main(void)
 {
     int num;
     while(!feof(stdin)) {
         printf("Zadejte cislo od 0 do 3999:\n");
-        if (scanf("%d", &num) != 1 || num < 0 || num > 3999 || getchar() != '\n') return error();
-        if (num <= 10) less_ten(num);
-        else if (num <= 50 && num > 10) less_fifty(num);
-        else if (num<=100 && num >50) less_hundred(num);
-        else if (num<=500 && num >100)less_5hundred(num);
-        else if (num<=1000 && num >500) less_thousand(num);
-        else if (num<4000 && num >1000) less_4thousand(num);
+        if (scanf("%d", &num) != 1 || num < 1 || num > 3999 || getchar() != '\n') return error();
+        itinerary(num);
         printf("\n\n");
     }
     return 0;
