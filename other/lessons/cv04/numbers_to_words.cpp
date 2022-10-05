23b444ef99dfa8d2441cb999909b984568c2bb2b
Sofiia Prykhach <prykhsof@fit.cvut.cz>
2022-10-05 14:41:24 +0200

    cv04: num to words done

--- other/lessons/cv04/numbers_to_words.cpp
+++ other/lessons/cv04/numbers_to_words.cpp
@@ -3,6 +3,20 @@ int error (void) {
     printf("Nespravny vstup.\n");
     return 0;
 }
+void less_ten (int num);
+void less_twenty( int num);
+void less_hundred(int num);
+void less_thousand(int num);
+void less_10thousand (int num);
+
+void itinerary(int num)
+{
+    if (num < 10) less_ten(num);
+    else if (num < 20) less_twenty(num);
+    else if (num< 100) less_hundred(num);
+   else if (num< 1000) less_thousand(num);
+   else less_10thousand(num);
+}
 
 void less_ten (int num)
 {
@@ -33,15 +47,14 @@ void less_ten (int num)
 
 void less_twenty( int num)
 {
-    num = num %10;
-    switch (num)
+    switch (num%10)
     {
         case 9:
         case 8:
         case 7:
         case 6:
         case 3:
-        case 2: less_ten(num); printf("nact");
+        case 2: itinerary(num%10); printf("nact");
             break;
         case 5: printf("patnact");
             break;
@@ -56,28 +69,64 @@ void less_twenty( int num)
 
 void less_hundred(int num)
 {
-    switch (num) {
-        case 9: printf("devadesat");
+    switch (num/10) {
+        case 9: printf("devadesat ");
             break;
-        case 6:printf("sedesat");
+        case 6:printf("sedesat ");
             break;
         case 5:
-            printf("padesat");
+            printf("padesat ");
             break;
 
         case 8:
         case 7:
-            less_ten(num);
-            printf("desat");
+            itinerary(num/10);
+            printf("desat ");
             break;
 
         case 3:
         case 4:
         case 2:
-            less_ten(num);
-            printf("cet");
+            itinerary(num/10);
+            printf("cet ");
             break;
     }
+    if(num%10 !=0) itinerary(num%10);
+}
+
+void less_thousand(int num)
+{
+    switch (num/100) {
+        case 9:
+        case 8:
+        case 7:
+        case 6:
+        case 5:
+            itinerary(num/100);
+            printf(" set ");
+            break;
+
+        case 3:
+        case 4:
+            itinerary(num/100);
+            printf(" sta ");
+            break;
+
+        case 2:
+            printf("dve ste ");
+            break;
+        case 1:  printf("sto ");
+            break;
+
+    }
+    if(num%10 !=0) itinerary(num%100);
+}
+
+void less_10thousand (int num)
+{
+    itinerary(num/1000);
+    printf(" tisic ");
+    itinerary(num%1000);
 
 }
 int main(void)
@@ -87,9 +136,7 @@ int main(void)
         printf("Zadejte cislo od 0 do 9999:\n");
         if (scanf("%d", &num) != 1 || num < 0 || num > 9999 || getchar() != '\n') return error();
 
-        if (num < 10) less_ten(num);
-        else if (num < 20 && num >= 10) less_twenty(num);
-        else if (num<100 && num >20) less_hundred(num);
+        itinerary(num);
         printf("\n\n");
     }
     return 0;
