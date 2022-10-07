b0525b9c945f27a78c5b6dc2a07b280f3ee800ae
Sofiia Prykhach <prykhsof@fit.cvut.cz>
2022-10-07 20:49:03 +0200

    cv05: balik is done (i still dont know what is it and i dont want to know)

--- other/lessons/cv05/balik.cpp
+++ other/lessons/cv05/balik.cpp
@@ -11,13 +11,8 @@ void print_sign(int l, char c) {
         --l;
     }
 }
-
-void print_bal (int h)
+void print_head(int full_h )
 {
-
-    int full_h = h*3;
-    if(h%2==0)full_h++;
-
     print_sign((full_h-5)/2 +1, ' ');
     printf("0%%0");
     printf("\n");
@@ -28,33 +23,50 @@ void print_bal (int h)
     printf("\\");
     print_sign((full_h-5)/2, '*');
     printf("\n");
-
-    for (int i=0; i<full_h; ++i) {
-        if (h%2==0 &&  (i==full_h/2 || i==(full_h/2)+2)) printf("|");
-        else if (h%2!=0 && i==full_h/2 ) printf("|");
-        else if (h%2!=0 &&  i==full_h/2 +3 ) printf("\\");
-        else if (i==(full_h-5)/2-1) printf("/");
-        else if (i ==0 || i==full_h-1) print_sign(1, '*');
-        else print_sign(1, ' ');
-    }
-    printf("\n");
-    for (int i=0; i<full_h; ++i) {
-        if (h%2==0 &&  (i==(full_h-5)/2-1|| i==full_h/2)) printf("|");
-        else if(h%2!=0 &&  (i==full_h/2 + 3 || i==full_h/2))  printf("|");
-        else if (h%2!=0 &&  i==(full_h-5)/2 -2) printf("/");
-        else if (i ==0 || i==full_h-1) print_sign(1, '*');
-        else print_sign(1, ' ');
-    }
-    printf("\n");
-    for (int i=0; i<h-2; ++i) {
+}
+void print_free_line ( int count, int full_h)
+{
+    for (int  i=0; i<count; ++i) {
         for (int j=0; j<full_h; ++j) {
-            if(j==0 || j == full_h-1) print_sign(1,'*');
-            else if(h%2!=0 &&  i==0 && j==(full_h-5)/2-2)  printf("|");
+            if(j==0 || j == full_h - 1) print_sign(1,'*');
             else if(j==full_h/2) printf("|");
             else print_sign(1, ' ');
         }
         printf("\n");
-        }
+    }
+}
+void print_arrow(int space) {
+
+    int j = 0;
+
+    while (space > 0) {
+        print_sign(1, '*');
+        print_sign(space, ' ');
+        if (space == 1)printf("|");
+        else printf("/");
+        print_sign(2 + j, ' ');
+        printf("|");
+        print_sign(1 + j, ' ');
+        if (space == 2)printf(" |");
+        else if (space == 1) print_sign(2, ' ');
+        else printf(" \\");
+        print_sign(space, ' ');
+        print_sign(1, '*');
+        printf("\n");
+        space--;
+        j++;
+
+    }
+}
+
+void print_bal (int h)
+{
+
+    int full_h = h*3;
+    if(h%2==0)full_h++;
+    print_head(full_h);
+    print_arrow((full_h-5)/2 -2);
+    print_free_line(h-2, full_h);
 
     for (int i=0; i<full_h; ++i) {
         if (i==full_h/2) printf("+");
@@ -63,14 +75,7 @@ void print_bal (int h)
     }
     printf("\n");
 
-    for (int i=0; i<h+1; ++i) {
-        for (int j=0; j<full_h; ++j) {
-            if(j==0 || j == full_h-1) print_sign(1,'*');
-            else if(j==full_h/2) printf("|");
-            else print_sign(1, ' ');
-        }
-        printf("\n");
-    }
+    print_free_line(h+1, full_h);
     print_sign(full_h, '*');
     printf("\n");
 }
