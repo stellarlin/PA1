10595729db5b1f12637132d85a6ff5aab304c171
Sofiia Prykhach <prykhsof@fit.cvut.cz>
2022-11-25 20:46:12 +0100

    pr05: little sadness and sam doma

--- progtest/prog05/airlines.cpp
+++ progtest/prog05/airlines.cpp
@@ -25,34 +25,35 @@ void initial_data (AIRPLANE_COORDINATE ** airpl_data, int max)
     if(*airpl_data==NULL) initial_data(airpl_data, max);
 }
 
-void realloc_data(AIRPLANE_COORDINATE *data, AIRPLANE_COORDINATE **data_realloc, int count) {
-    *data_realloc= (AIRPLANE_COORDINATE*)realloc(data, count * sizeof(AIRPLANE_COORDINATE));
-    if(*data_realloc==NULL)realloc_data(data, data_realloc, count);
-    *data=**data_realloc;
+void realloc_data(AIRPLANE_COORDINATE **data, AIRPLANE_COORDINATE **data_realloc,  int * max) {
+    *data_realloc= (AIRPLANE_COORDINATE*)realloc(*data, (*max) * 2 * sizeof(AIRPLANE_COORDINATE));
+    if(*data_realloc==NULL)realloc_data(data, data_realloc,  max);
+    *max*=2;
+    *data=*data_realloc;
 }
 
 bool read_coordinate(AIRPLANE_COORDINATE * airpl_data, int * count) {
 
 
-    if(scanf(" %lf, %lf :  %199s", &airpl_data->x, &airpl_data->y, airpl_data->name)!=3) return false;
+    if(scanf(" %lf, %lf :  %199s", &airpl_data[*count].x, &airpl_data[*count].y, airpl_data[*count].name)!=3) return false;
         ++*count;
     return true;
 }
 bool dbl_eps_check( double a, double b)
 {
-    return fabs(a-b)<=DBL_EPSILON*100*b;
+    return fabs(a-b)>=DBL_EPSILON*100*(fabs(b) + fabs(a));
 }
+
 
 double distance (AIRPLANE_COORDINATE first, AIRPLANE_COORDINATE next, double * min)
 {
     double dis = sqrt(pow(first.x-next.x, 2) + pow(first.y-next.y, 2));
-    if(fabs(*min-dis)<=DBL_EPSILON*100*dis) *min=dis;
-    if(dbl_eps_check(*min, dis)) *min=dis;
+    if(dbl_eps_check(*min, dis) && dis<*min) *min=dis;
     return dis;
 }
 void swap (AIRPLANE_COORDINATE * x, AIRPLANE_COORDINATE * y, AIRPLANE_COORDINATE * z)
 {
-    AIRPLANE_COORDINATE tmp;
+    AIRPLANE_COORDINATE  tmp;
     tmp = *x;
     *x = *y;
     *y=*z;
@@ -65,7 +66,7 @@ void sort_data(AIRPLANE_COORDINATE * airpl_data, int count, double * min_distanc
     double diff_1, diff_2;
     int start = 0;
     int end = count - 2;
-
+    *min_distance =distance(airpl_data[0], airpl_data[1], min_distance);
     while(swapped)
     {
         swapped = false;
@@ -73,7 +74,7 @@ void sort_data(AIRPLANE_COORDINATE * airpl_data, int count, double * min_distanc
             diff_1=distance(airpl_data[i], airpl_data[i + 1], min_distance);
             diff_2= distance(airpl_data[i+1], airpl_data[i + 2], min_distance);
             if (dbl_eps_check(diff_2,
-                             diff_1)) {
+                             diff_1) && diff_2<diff_1) {
                 swap(&airpl_data[i], &airpl_data[i + 1], &airpl_data[i + 2]);
                 swapped = true;
             }
@@ -92,7 +93,7 @@ void sort_data(AIRPLANE_COORDINATE * airpl_data, int count, double * min_distanc
             diff_2= distance(airpl_data[i+1], airpl_data[i + 2], min_distance);
 
             if (dbl_eps_check(diff_2,
-                              diff_1))
+                              diff_1) || diff_2 < diff_1)
             {
                 swap(&airpl_data[i], &airpl_data[i + 1], &airpl_data[i + 2]);
                 swapped = true;
@@ -111,10 +112,10 @@ int main (void)
     printf("Pozice letadel:\n");
     while(1)
     {
-        if(!read_coordinate(airpl_data, &count) || (c=getchar())==EOF || c=='\n') break;
-        if(count>=max) realloc_data(airpl_data, &airpl_realloc, count);
+        if(!read_coordinate(airpl_data, &count) || (c=getchar())==EOF || c=='/') break;
+        if(count+1>=max) realloc_data(&airpl_data, &airpl_realloc, &max);
     }
-    if(!feof(stdin) || count<2) return error(&airpl_data);
+    if((!feof(stdin) && c!='\n') || count<2) return error(&airpl_data);
     sort_data(airpl_data, count, &min_distance);
     free(airpl_data);
     return 0;
