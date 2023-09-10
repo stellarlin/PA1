a6e7d1b0ccbbe42b5d718c1819c18a6b86dd49b4
Sofiia Prykhach <88281550+stellarlin@users.noreply.github.com>
2023-09-10 21:55:45 +0200

    pr00: future loop base, sentence output function

--- progtest/prog00/quotes.cpp
+++ progtest/prog00/quotes.cpp
@@ -1,5 +1,5 @@
-#include<stdio.h>
-#include <ctype.h>
+#include<cstdio>
+#include <cctype>
 
 // Stop the program and show error message
 int error (void) {
@@ -8,20 +8,62 @@ int error (void) {
 }
 
 // show the sentence with number 1-9
-void sentence (const int n)
+bool sentence (const int n)
 {
+    switch(n)
+    {
+        case 1: printf("Qapla'\n"
+                       "noH QapmeH wo' Qaw'lu'chugh yay chavbe'lu' 'ej wo' choqmeH may' DoHlu'chugh lujbe'lu'.\n");
+            break;
+        case 2: printf("Qapla'\nbortaS bIr jablu'DI' reH QaQqu' nay'.\n");
+            break;
+        case 3: printf("Qapla'\n"
+                       "Qu' buSHa'chugh SuvwI', batlhHa' vangchugh, qoj matlhHa'chugh, pagh ghaH SuvwI''e'.\n");
+            break;
+        case 4: printf("Qapla'\n"
+                       "bISeH'eghlaH'be'chugh latlh Dara'laH'be'.\n");
+            break;
+        case 5: printf("Qapla'\n"
+                       "qaStaHvIS wa' ram loS SaD Hugh SIjlaH qetbogh loD.\n");
+            break;
+        case 6: printf("Qapla'\n"
+                       "Suvlu'taHvIS yapbe' HoS neH.\n");
+            break;
+        case 7: printf("Qapla'\n"
+                       "Ha'DIbaH DaSop 'e' DaHechbe'chugh yIHoHQo'.\n");
+            break;
+
+        case 8: printf("Qapla'\n"
+                       "Heghlu'meH QaQ jajvam.\n");
+            break;
+
+        case 9: printf("Qapla'\n"
+                       "leghlaHchu'be'chugh mIn lo'laHbe' taj jej.\n");
+        default: return false;
+    }
+    return true;
 }
 
-// check if sentence index is correct
-bool isValid (const int n)
+bool isWhite (void)
 {
-    return (n<1 || n>9 );
+    char c;
+    while ((c = getchar())!=EOF )
+    {
+        if(!isspace(c)) return false;
+    }
+    return true;
 }
-
 
 int main(void)
 {
-    int n;
+    double n;
+    // future loop base
+    printf("ml' nob:\n");
+    if(scanf(" %lf", &n)!=1 // input has less then ome correct integer
+       || !isWhite() // after number of sentence there is another text
+       || !sentence(n) // number is more then 9 or less then 1
+       ) return error();
     return 0;
 }
+
 
