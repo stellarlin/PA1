a6ad06d4218599f078025b0c32477e6593db5691
Sofiia Prykhach <88281550+stellarlin@users.noreply.github.com>
2023-09-10 21:40:48 +0200

    pr00: program base

---
+++ progtest/prog00/quotes.cpp
@@ -0,0 +1,27 @@
+#include<stdio.h>
+#include <ctype.h>
+
+// Stop the program and show error message
+int error (void) {
+    printf("luj\n");
+    return 0;
+}
+
+// show the sentence with number 1-9
+void sentence (const int n)
+{
+}
+
+// check if sentence index is correct
+bool isValid (const int n)
+{
+    return (n<1 || n>9 );
+}
+
+
+int main(void)
+{
+    int n;
+    return 0;
+}
+
