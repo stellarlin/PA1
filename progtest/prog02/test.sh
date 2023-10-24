60eea42a43f9947eb62b42e35c42977a7771c7e8
Sofiia Prykhach <prykhsof@fit.cvut.cz>
2023-10-25 01:42:36 +0200

    pr02: tests

---
+++ progtest/prog02/test.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+for IN_FILE in tests/00{00..99} ; do
+  if [ ! -e "${IN_FILE}_in.txt" ] ; then
+    echo "All OK"
+    exit 0
+  fi
+  ./a.out<"${IN_FILE}_in.txt" > /tmp/out.txt
+  if ! diff -q "${IN_FILE}_out.txt" /tmp/out.txt ; then
+    echo "Mismatch ${IN_FILE}_in.txt"
+    exit 1
+  fi
+done
