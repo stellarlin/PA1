485fccccb8c1bcdd8ad3b3e9306f6f1aa06b7f37
Sofiia Prykhach <prykhsof@fit.cvut.cz>
2023-11-02 18:13:02 +0100

    pr02: working days counter correct version

--- progtest/prog03/working_days_counter.cpp
+++ progtest/prog03/working_days_counter.cpp
@@ -3,8 +3,7 @@
 #include <stdlib.h>
 #include <assert.h>
 
-#define starting_year 2000
-#define YEAR 365
+
 typedef struct
 {
   int m_TotalDays;
@@ -13,6 +12,8 @@ typedef struct
 #endif /* __PROGTEST__ */
 
 
+#define starting_year 2000
+#define YEAR 365
 
 struct DATE
 {
@@ -38,7 +39,7 @@ bool isHoliday (DATE * date)
         case 5: return date->day == 1 || date->day == 8;
         case 7: return date->day == 5 || date->day == 6;
 
-        case 9:
+        case 9:  return date->day == 28;
         case 10: return date->day == 28;
 
         case 11: return date->day == 17;
@@ -59,8 +60,9 @@ int dayOfWeek (DATE date)
 
     int century = date.year / 100;
     int reminder = date.year % 100;
-    return (date.day  + 13 * (date.month  + 1) / 5
-    + reminder + reminder / 4 + century / 4 - 2 * century) % 7;
+    int h = date.day  + 13 * (date.month  + 1) / 5 + reminder + reminder / 4 + century / 4 - 2 * century;
+
+    return ((h + 5) % 7 + 7) % 7 + 1;
 
 }
 
@@ -113,10 +115,20 @@ bool isValidDate (struct DATE * date)
     && date->day >=1 && date->day <=month_days(date->month, date->year);
 }
 
+bool isWeekend ( int y, int m, int d)
+{
+    DATE date (y, m, d);
+    return dayOfWeek(date) > 5;
+}
+
+bool isWeekend ( DATE * date)
+{
+    return dayOfWeek(*date) > 5;
+}
+
 bool isWorkDay ( DATE * date)
 {
-    int day_of_week = dayOfWeek(*date);
-    return ( day_of_week > 1 && !isHoliday(date));
+    return (!isWeekend(date) && !isHoliday(date));
 }
 
 bool isWorkDay ( int y, int m, int d )
@@ -127,41 +139,41 @@ bool isWorkDay ( int y, int m, int d )
      return (isWorkDay(&date));
 }
 
+
 TResult countDays ( int y1, int m1, int d1,
                     int y2, int m2, int d2 ) {
     TResult result;
-    result.m_TotalDays = 0;
-    result.m_WorkDays = 0;
+    result.m_TotalDays = -1;
+    result.m_WorkDays = -1;
     DATE start(y1, m1, d1), end(y2, m2, d2);
 
 
-    if (!isValidDate(&start) || !isValidDate(&end)
-        || !isValidInterval(&start, &end)) {
-        result.m_TotalDays = -1;
-        result.m_WorkDays = -1;
-        return result;
-    }
+    if (!isValidDate(&start) || !isValidDate(&end)) return result;
+    if(!isValidInterval(&start, &end)) return result;
 
+    result.m_TotalDays = 0;
+    result.m_WorkDays = 0;
 
-    while (start.year != end.year || start.month != end.month || start.day != end.day) {
-        result.m_TotalDays++;
-        if (isWorkDay(&start)) result.m_WorkDays++;
+        while (start.year != end.year || start.month != end.month || start.day != end.day) {
+            result.m_TotalDays++;
+            if (isWorkDay(&start)) result.m_WorkDays++;
 
-        start.day++;
-        if (start.day > month_days(start.month, start.year)) {
-            start.day = 1;
-            start.month++;
-         }
+            start.day++;
+            if (start.day > month_days(start.month, start.year)) {
+                start.day = 1;
+                start.month++;
+            }
 
-        if (start.month > 12) {
-            start.month = 1;
-            start.year++;
-         }
-}
+            if (start.month > 12) {
+                start.month = 1;
+                start.year++;
+            }
+        }
+
 
     // Include the end date in the count
     result.m_TotalDays++;
-    if (isWorkDay(&start)) {
+    if (isWorkDay(&end)) {
         result.m_WorkDays++;
     }
     return result;
@@ -171,73 +183,87 @@ TResult countDays ( int y1, int m1, int d1,
 int main ( int argc, char * argv [] )
 {
   TResult r;
-    r = countDays(2052, 01, 01, 2052, 12, 31);
-    printf("Total: %d\nWorkDays: %d\n", r.m_TotalDays, r.m_WorkDays);
+    /*
+      auto d = dayOfWeek(DATE( 2000,  3,  1));
+      printf("It's %d!\n", d);
+     // r = countDays(2004, 01, 01, 2005, 01, 01);
+    //  printf("Total: %d\nWorkDays: %d\n", r.m_TotalDays, r.m_WorkDays);
 
+      d = !isWeekend(2000, 01, 01) + !isWeekend(2000, 05, 01) + !isWeekend(2000, 05, 8)
+              + !isWeekend(2000, 07, 5) +  !isWeekend(2000, 07, 6)  + !isWeekend(2000, 9, 28)
+              + !isWeekend(2000, 10, 28) + !isWeekend(2000, 11, 17) + !isWeekend(2000, 12, 24)
+              + !isWeekend(2000, 12, 25) + !isWeekend(2000, 12, 26);
+      printf("WorkDays: %d\n", 260 - d);
+  */
 
-  DATE d (1,01,2023);
- assert ( isWorkDay ( 2023, 10, 10 ) );
+     assert ( isWorkDay ( 2023, 10, 10 ) );
 
- assert ( ! isWorkDay ( 2023, 11, 11 ) );
+     assert ( ! isWorkDay ( 2023, 11, 11 ) );
 
- assert ( ! isWorkDay ( 2023, 11, 17 ) );
+     assert ( ! isWorkDay ( 2023, 11, 17 ) );
 
- assert ( ! isWorkDay ( 2023, 11, 31 ) );
+     assert ( ! isWorkDay ( 2023, 11, 31 ) );
 
- assert ( ! isWorkDay ( 2023,  2, 29 ) );
+     assert ( ! isWorkDay ( 2023,  2, 29 ) );
 
- assert ( ! isWorkDay ( 2004,  2, 29 ) );
+     assert ( ! isWorkDay ( 2004,  2, 29 ) );
 
- assert ( isWorkDay ( 2008,  2, 29 ) );
+     assert ( isWorkDay ( 2008,  2, 29 ) );
 
-  assert ( ! isWorkDay ( 2001,  2, 29 ) );
+      assert ( ! isWorkDay ( 2001,  2, 29 ) );
 
-  assert ( ! isWorkDay ( 1996,  1,  2 ) );
+      assert ( ! isWorkDay ( 1996,  1,  2 ) );
 
-  r = countDays ( 2023, 11,  1,
-                  2023, 11, 30 );
-  assert ( r . m_TotalDays == 30 );
-  assert ( r . m_WorkDays == 21 );
+      r = countDays ( 2023, 11,  1,
+                      2023, 11, 30 );
+      assert ( r . m_TotalDays == 30 );
+      assert ( r . m_WorkDays == 21 );
 
-  r = countDays ( 2023, 11,  1,
-                  2023, 11, 17 );
-  assert ( r . m_TotalDays == 17 );
-  assert ( r . m_WorkDays == 12 );
+      r = countDays ( 2023, 11,  1,
+                      2023, 11, 17 );
+      assert ( r . m_TotalDays == 17 );
+      assert ( r . m_WorkDays == 12 );
 
-  r = countDays ( 2023, 11,  1,
-                  2023, 11,  1 );
-  assert ( r . m_TotalDays == 1 );
-  assert ( r . m_WorkDays == 1 );
+      r = countDays ( 2023, 11,  1,
+                      2023, 11,  1 );
+      assert ( r . m_TotalDays == 1 );
+      assert ( r . m_WorkDays == 1 );
 
-  r = countDays ( 2023, 11, 17,
-                  2023, 11, 17 );
-  assert ( r . m_TotalDays == 1 );
-  assert ( r . m_WorkDays == 0 );
+      r = countDays ( 2023, 11, 17,
+                      2023, 11, 17 );
+      assert ( r . m_TotalDays == 1 );
+      assert ( r . m_WorkDays == 0 );
 
-  r = countDays ( 2023,  1,  1,
-                  2023, 12, 31 );
-  assert ( r . m_TotalDays == 365 );
-  assert ( r . m_WorkDays == 252 );
+      r = countDays ( 2023,  1,  1,
+                      2023, 12, 31 );
+      assert ( r . m_TotalDays == 365 );
+      assert ( r . m_WorkDays == 252 );
 
-  r = countDays ( 2024,  1,  1,
-                  2024, 12, 31 );
-  assert ( r . m_TotalDays == 366 );
-  assert ( r . m_WorkDays == 254 );
-/*
-  r = countDays ( 2000,  1,  1,
-                  2023, 12, 31 );
-  assert ( r . m_TotalDays == 8766 );
-  assert ( r . m_WorkDays == 6072 );
+      r = countDays ( 2024,  1,  1,
+                      2024, 12, 31 );
+      assert ( r . m_TotalDays == 366 );
+      assert ( r . m_WorkDays == 254 );
 
-  r = countDays ( 2001,  2,  3,
-                  2023,  7, 18 );
-  assert ( r . m_TotalDays == 8201 );
-  assert ( r . m_WorkDays == 5682 );
-*/
+    r = countDays ( 2000,  1,  1,
+                    2023, 12, 31 );
+    assert ( r . m_TotalDays == 8766 );
+    assert ( r . m_WorkDays == 6072 );
+
+    r = countDays ( 2001,  2,  3,
+                    2023,  7, 18 );
+    assert ( r . m_TotalDays == 8201 );
+    assert ( r . m_WorkDays == 5682 );
+
   r = countDays ( 2021,  3, 31,
                   2023, 11, 12 );
   assert ( r . m_TotalDays == 957 );
   assert ( r . m_WorkDays == 666 );
+
+    r = countDays ( 2021,  3, 31,
+                    2021, 3, 31 );
+    assert ( r . m_TotalDays == 1 );
+    assert ( r . m_WorkDays == 1 );
+
 
   r = countDays ( 2001,  1,  1,
                   2000,  1,  1 );
