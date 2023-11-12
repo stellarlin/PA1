8d90c0f90f7236dd820e88e7ee2d5aed1f00c190
Sofiia Prykhach <prykhsof@fit.cvut.cz>
2023-11-12 21:38:56 +0100

    pr04: histograms added

--- progtest/prog04/castle.cpp
+++ progtest/prog04/castle.cpp
@@ -9,39 +9,71 @@ constexpr int MAP_MAX = 200;
 struct Square
 {
     Square (int X, int Y) : x(X), y(Y) {}
-    bool equal (Square  other) { return x == other.x && y == other.y;}
-    bool under (Square other) { return y > other.y;}
-    bool above (Square other) { return !under(other);}
-    bool right (Square other) {return x < other.x;}
-    bool left (Square other) { return !right(other);}
-
     int x;
     int y;
 };
 
-struct Borders
+struct Borders{
+explicit Borders(Square castle) : west (castle.x), east (castle.x), north(castle.y), south (castle.y) {}
+   int height () const {return  south - north + 1;}
+    int width () const { return  east - west + 1;}
+int west;
+int east;
+int north;
+int south;
+};
+
+struct Stack
 {
-    Borders(Square castle) : west (castle.x), east (castle.x), north(castle.y), south (castle.y) {}
-    int area () const { return (east - west + 1) * (south - north + 1);}
-    int west;
-    int east;
-    int north;
-    int south;
+    int top = -1;
+    int capacity;
+    int * array;
+
+
+    bool full () const{return top == capacity - 1;}
+    bool empty () const {return top == -1;}
+
+    void push (int item ) {
+        if (full()) return;
+        array[++top] = item;
+    }
+    void pop ()
+    {
+        if (empty()) return;
+        top--;
+    }
 
 };
 
+Stack* createStack(int capacity)
+{
+    Stack * stack = (struct Stack *) malloc (sizeof(Stack));
+    stack->top = -1;
+    stack->capacity = capacity;
+    stack->array = (int *) malloc (stack->capacity * sizeof(int));
+    return stack;
+}
+
+void deleteStack (Stack ** stack)
+{
+    free ((*stack)->array);
+    free (*stack);
+}
 
 
 bool underCastle (Square  square, Square  castle, int altitude[][MAP_MAX])
 {
-    return altitude[castle.y][castle.x] > altitude [square.y][square.x];
+    return (castle.x == square.x && castle.y == square.y) ||
+           altitude[castle.y][castle.x] > altitude[square.y][square.x];
 }
 
-bool insideKingdom (Borders * kingdom, Square  square)
-{
-    return square.x >= kingdom->north && square.x <= kingdom->south
-           && square.y >= kingdom->west && square.y <= kingdom->east;
-}
+int max (int a, int b)
+{return a >= b ? a : b;}
+
+
+// Finds the maximum area under
+// the histogram represented
+// by histogram. See below article for details.
 
 void defineCross (int altitude[][MAP_MAX], Square castle,   Borders * kingdom, int size)
 {
@@ -51,21 +83,18 @@ void defineCross (int altitude[][MAP_MAX], Square castle,   Borders * kingdom, i
         if (!underCastle({castle.x, y}, castle, altitude)) break;
         kingdom->north--;
     }
-
     //define South
     for (int y = castle.y + 1; y <  size; y++)
     {
         if (!underCastle({castle.x, y}, castle, altitude)) break;
         kingdom->south++;
     }
-
     //define West
     for (int x = castle.x - 1; x >= 0; x--)
     {
         if (!underCastle({x, castle.y}, castle, altitude)) break;
         kingdom->west--;
     }
-
     //define East
     for (int x = castle.x + 1; x <  size; x++)
     {
@@ -74,61 +103,98 @@ void defineCross (int altitude[][MAP_MAX], Square castle,   Borders * kingdom, i
     }
 }
 
-bool isCross (Square castle, Square square)
+
+
+int maxHist(int row[], Square * castle, Borders * Kingdom)
 {
-    return castle.x == square.x || castle.y == square.y;
-}
+    // Create an empty stack.
+    Stack * stack = createStack(Kingdom->width());
 
+    //----------Initialization-----------------------
+    bool capital_found = false; // true when we detected bar
+                                // that consists castle
+    int max_area = 0;
+    int curr_area;
+    int x = 0;
+    //----------------------------------------------
+    // Run through all bars of given histogram (or row)
 
-void changeBorder ( Square castle, Square wrongSquare, Borders * kingdom)
-{
-    if (wrongSquare.under(castle) && wrongSquare.right(castle))
-    {
-        kingdom->east -= kingdom->east > wrongSquare.x ? 1 : 0;
-        kingdom->south -= kingdom->south > wrongSquare.y ? 1: 0;
-    }
-    else if (wrongSquare.under(castle) && wrongSquare.left(castle))
-    {
-        kingdom->west += kingdom->west > wrongSquare.x ? 1 : 0;
-        kingdom->south -= kingdom->south > wrongSquare.y ? 1: 0;
-    }
-    else if (wrongSquare.above(castle) && wrongSquare.right(castle))
-    {
-        kingdom->east -= kingdom->east > wrongSquare.x ? 1 : 0;
-        kingdom->north += kingdom->north > wrongSquare.y ? 1: 0;
-    }
-    else
-    {
-        kingdom->west += kingdom->west > wrongSquare.x ? 1 : 0;
-        kingdom->north += kingdom->north > wrongSquare.y ? 1: 0;
-    }
-}
-
-void checkArea (int altitude[][MAP_MAX], Square castle,   Borders * kingdom, int size)
-
-{
-    for (int y = kingdom->north; y <= kingdom->south; y++)
-    {
-        for (int x = kingdom->west; x <= kingdom->east; x++)
+    while (x <  Kingdom->width()) {
+        // If this bar is higher than the bar on top stack,
+        // push it to stack
+        if (stack->empty()) stack->push(x++);
+        else if (row[stack->top] <= row[x])
         {
-            if (insideKingdom(kingdom, {x,y}) || isCross (castle, {x,y}) ) continue;
-            if (!underCastle( {x,y}, castle, altitude))
-                changeBorder(castle, {x,y}, kingdom);
+            if (row[x] == 0 && capital_found) break;
+            if (x == castle->x) capital_found = true;
+            stack->push(x++);
+
+        }
+
+        else {
+            int & top_val = row[stack->array[stack->top]];
+            stack->pop();
+
+            if (row[x] == 0 && capital_found) break;
+
+            curr_area = top_val * x;
+            if (!stack->empty()) curr_area = top_val * (x - stack->top - 1);
+            max_area = max(curr_area, max_area);
+            if (x == castle->x) capital_found = true;
         }
     }
 
+    // Now pop the remaining bars from stack and calculate
+    // area with every popped bar as the smallest bar
+    while (!stack->empty()) {
+        int & top_val = row[stack->array[stack->top]];
+        stack->pop();
+        curr_area = top_val * x;
+        if (!stack->empty())
+            curr_area = top_val * (x - stack->top - 1);
+
+        max_area = max(curr_area, max_area);
+    }
+// free memory
+    deleteStack(&stack);
+    return max_area;
 }
 
 
 int calculateKingdom (int altitude[][MAP_MAX], Square castle, int size)
 {
+    if (!altitude[castle.y][castle.x]) return 1;
 
     //define borders
     Borders Kingdom(castle);
     defineCross(altitude, castle, &Kingdom, size);
-    checkArea(altitude, castle, &Kingdom, size);
 
-    return Kingdom.area();
+    int histograms [MAP_MAX][MAP_MAX];
+
+    //copy first row
+    for (int i  = 0; i<= Kingdom.width(); i++)
+    {
+        histograms[0][i] = underCastle({i + Kingdom.west, Kingdom.north }, castle, altitude);
+    }
+    //calculate the area of 1 row
+    int max_area = maxHist(histograms[0], &castle, &Kingdom);
+    //transform rows in as follows, if histograms[i][j]
+    // is not zero then histograms[i][j] = histograms[i-1][j] + histograms[i][j].
+    for (int y =  1; y < Kingdom.height(); y++) {
+
+        for (int x = 0; x < Kingdom.width(); x++)
+        {
+            // if A[i][j] is 1 then add A[i -1][j]
+            histograms[y][x] =  underCastle({x + Kingdom.west, y + Kingdom.north}, castle, altitude)
+                    ? (1 + histograms[y - 1][x]) : 0;
+        }
+
+        // Update result  using "Largest Rectangular Area in a Histogram"
+        // problem solutionl
+        max_area = max(max_area, maxHist(histograms[y], &castle, &Kingdom));
+
+    }
+    return max_area;
 }
 
 void castleArea ( int altitude[][MAP_MAX], int size, int area[][MAP_MAX] )
@@ -152,14 +218,13 @@ void printArea ( int area[][MAP_MAX], int size)
         }
         printf ("\n");
     }
+    printf ("\n");
 }
 
 
 #ifndef __PROGTEST__
 bool identicalMap ( const int a[][MAP_MAX], const int b[][MAP_MAX], int n )
 {
-
-    return true;
   for (int i = 0; i < n; i++) {
   for (int j = 0; j < n; j ++) if (a [i][j] != b[i][j]) return false;
   }
@@ -169,7 +234,7 @@ bool identicalMap ( const int a[][MAP_MAX], const int b[][MAP_MAX], int n )
 int main ( int argc, char * argv [] )
 {
   static int result[MAP_MAX][MAP_MAX];
-
+/*
   static int alt0[MAP_MAX][MAP_MAX] =
   {
     { 1, 2 },
@@ -181,9 +246,9 @@ int main ( int argc, char * argv [] )
     { 2, 4 }
   };
   castleArea ( alt0, 2, result );
-  printArea(result, 2);
+  //printArea(result, 2);
   assert ( identicalMap ( result, area0, 2 ) );
-  static int alt1[MAP_MAX][MAP_MAX] =
+*/  static int alt1[MAP_MAX][MAP_MAX] =
   {
     { 2, 7, 1, 9 },
     { 3, 5, 0, 2 },
@@ -198,7 +263,7 @@ int main ( int argc, char * argv [] )
     { 1, 2, 1, 12 }
   };
   castleArea ( alt1, 4, result );
-  printArea(result, 4);
+  //printArea(result, 4);
   assert ( identicalMap ( result, area1, 4 ) );
   static int alt2[MAP_MAX][MAP_MAX] =
   {
@@ -215,6 +280,7 @@ int main ( int argc, char * argv [] )
     { 4, 8, 12, 16 }
   };
   castleArea ( alt2, 4, result );
+    //printArea(result, 4);
   assert ( identicalMap ( result, area2, 4 ) );
   static int alt3[MAP_MAX][MAP_MAX] =
   {
@@ -231,6 +297,7 @@ int main ( int argc, char * argv [] )
     { 1, 2, 6, 12 }
   };
   castleArea ( alt3, 4, result );
+    //printArea(result, 4);
   assert ( identicalMap ( result, area3, 4 ) );
   static int alt4[MAP_MAX][MAP_MAX] =
   {
@@ -249,6 +316,7 @@ int main ( int argc, char * argv [] )
     { 1, 1, 1, 1, 1 }
   };
   castleArea ( alt4, 5, result );
+    //printArea(result, 5);
   assert ( identicalMap ( result, area4, 5 ) );
   return EXIT_SUCCESS;
 }
